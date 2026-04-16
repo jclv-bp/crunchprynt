32eadb9843bcd854dcbdc86b5b6a0bb42f6f32f4
jc <jc@bluprynt.com>
2026-04-16 16:37:37 -0600

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import { Inter } from "next/font/google";
+import { Inter, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={inter.className}>{children}</body>
     </html>
   );
